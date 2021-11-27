72690b69e097a8964f9df2582e4898273c4aa9f2
Vaishnavi Rangarajan <r.vaishnavi15@gmail.com>
2021-11-27 12:41:15 -0800

    Read/display CSV functionality added

--- index.R
+++ index.R
@@ -1,18 +1,25 @@
 library(shiny)
 library(shinythemes)
+library(DT)
 
 ui<- fluidPage(theme= shinytheme('yeti'),
                navbarPage("My Page",
                   tabPanel("Navbar",
                   sidebarPanel(
                      h3('Enter numbers'),
-                     textInput("num1", "Enter num1:",""),
-                     textInput("num2", "Enter num2:","")
+                     textInput("num1", "Enter num1:","23"),
+                     textInput("num2", "Enter num2:","34"),
+          
+                     fileInput(inputId = "filedata",
+                               label = "Upload data. Choose CSV file",
+                               accept = c(".csv"))
                     ),
                  mainPanel(
                    h1('Hello world'),
                    p('This is just another text!'),
-                   verbatimTextOutput("addnum")
+                   verbatimTextOutput("addnum"),
+                   h3("Display contents of CSV file"),
+                   dataTableOutput("textdata")
                  )
                 ),
                 tabPanel('Categories','Blank for now'),
@@ -22,9 +29,18 @@ ui<- fluidPage(theme= shinytheme('yeti'),
 
 
 server <- function(input, output, session) {
+  data <- reactive({
+    inFile <- input$filedata
+    if (is.null(inFile)) return(NULL)
+    read.csv(inFile$datapath)
+  })
+  
   output$addnum <- renderText({
     paste(as.numeric(input$num1)+as.numeric(input$num2))
   })
+  output$textdata<-renderDataTable(
+    data()
+  )
 }
 
 shinyApp(ui, server)
