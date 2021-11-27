60f1fbd1c37258c5f470321f9dc44949a0373c49
Vaishnavi Rangarajan <r.vaishnavi15@gmail.com>
2021-11-27 14:53:13 -0800

    Datatype extraction and display

--- index.R
+++ index.R
@@ -2,7 +2,11 @@ library(shiny)
 library(shinythemes)
 library(DT)
 
+
 ui<- fluidPage(theme= shinytheme('yeti'),
+             
+               
+               "Press any key",
                navbarPage("My Page",
                   tabPanel("Navbar",
                   sidebarPanel(
@@ -18,26 +22,40 @@ ui<- fluidPage(theme= shinytheme('yeti'),
                    h1('Hello world'),
                    p('This is just another text!'),
                    verbatimTextOutput("addnum"),
+                   h3("Datatypes available"),
+                   tableOutput("datatype.table"),
                    h3("Display contents of CSV file"),
                    dataTableOutput("textdata")
+                   
                  )
                 ),
                 tabPanel('Categories','Blank for now'),
                 tabPanel('Frequency','Blank for now')
                )
 )
-
+jscode <- "
+shinyjs.init = function() {
+  $(document).keypress(function(e) { alert('Key pressed: ' + e.which); });
+}"
 
 server <- function(input, output, session) {
+  
   data <- reactive({
     inFile <- input$filedata
     if (is.null(inFile)) return(NULL)
     read.csv(inFile$datapath)
+    df.textfile<- as.data.frame(read.csv(inFile$datapath))
   })
+ 
   
   output$addnum <- renderText({
     paste(as.numeric(input$num1)+as.numeric(input$num2))
+    #
+    #paste(data.type.file)
   })
+  output$datatype.table<-renderTable({
+    data.type.file<- as.list(sapply(data(), 'class'))
+    data.type.file})
   output$textdata<-renderDataTable(
     data()
   )
