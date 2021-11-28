f2a6eaa6fcbb8cc2157f59e27944b3232565d2eb
Vaishnavi Rangarajan <r.vaishnavi15@gmail.com>
2021-11-28 02:17:19 -0800

    Text highlighting added

--- index.R
+++ index.R
@@ -17,14 +17,16 @@ js <- HTML(
 ui <- fluidPage(
   tags$head(tags$script(js)),
   tags$script(
-    '
+    'let store = [];
                $(document).on("keypress", function (e) {
 
        if(e.which=="99"){
 
         text = window.getSelection().toString();
+store.push(text);
+Shiny.setInputValue("store", store);
 
-    alert(text)
+    alert(store.length);
        }
     });'
   ),
@@ -54,6 +56,7 @@ ui <- fluidPage(
         verbatimTextOutput("summary_stat"),
         h3("Datatypes available"),
         actionButton("convert", "Convert character to factor"),
+        
         tableOutput("datatype.table"),
         actionButton("text_data", "Show only text"),
         h3("Display contents of CSV file"),
@@ -62,8 +65,15 @@ ui <- fluidPage(
       )
     ),
     tabPanel('Categories',
-             
-             p('sample text hello world okay')),
+             h3('Sample Text'),
+             p('The Andromeda Galaxy (IPA: /ænˈdrɒmɪdə/), also known as Messier 31, M31, or NGC 224 and originally the Andromeda Nebula
+             (see below), is a barred spiral galaxy approximately 2.5 million light-years (770 kiloparsecs) from Earth and the nearest large galaxy to the Milky Way.[6] The galaxy\'s
+             name stems from the area of
+             Earth\'s sky in which it appears, the constellation of Andromeda, which itself is named after the Ethiopian (or Phoenician) princess who was the wife of
+             Perseus in Greek mythology.'),
+
+           h3('Text Parsed'),
+               verbatimTextOutput("store")),
     tabPanel('Frequency', 'Blank for now')
   )
 )
@@ -73,6 +83,7 @@ shinyjs.init = function() {
 }"
 
 server <- function(input, output, session) {
+  counter<-list()
   data <- reactive({
     inFile <- input$filedata
     if (is.null(inFile))
@@ -81,6 +92,9 @@ server <- function(input, output, session) {
     df.textfile <- as.data.frame(read.csv(inFile$datapath))
   })
   
+  output$store<-renderText({
+   paste(input$store, collapse = '\n')
+  })
   
   output$summary_stat <- renderPrint({
     # paste(as.numeric(input$num1)+as.numeric(input$num2))
